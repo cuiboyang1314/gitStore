5efa45145a7564e5acf66c24d68b776e6c4d4984
boring.Cui <cuiboyang1997@gmail.com>
2019-11-21 20:04:27 +0800

    nuct 3.0

--- nuct/components/nav.vue
+++ nuct/components/nav.vue
@@ -2,8 +2,12 @@
 <template>
       <ul class="nav">
           <li><a href="/">网站首页</a></li>
-          <li><a href="/about">关于我们</a></li>
-          <li><a>服务介绍</a></li>
+          <li><a>研究院介绍</a></li>
+          <li><a>机构设置</a></li>
+          <li><a>科学研究</a></li>
+          <li><a>教学课程</a></li>
+          <li><a>项目合作</a></li>
+          <li><a>成果推广</a></li>
           <li><a>联系我们</a></li>
       </ul>
 </template>
@@ -29,17 +33,17 @@ export default {
     .nav {
         position: absolute;
         top: 83px;
-        right: 185px;
+        right: 130px;
         height: 40px;
         z-index: 9999;
-        width: 600px;
+        width: 800px;
         height: 40px;
     }
 
     .nav li {
         text-align: center;
         float: left;
-        width: 100px;
+        width: 80px;
         height: 35px;
         line-height: 35px;
         margin: 0 10px;
